abd4c4f51efbb111f4ddffe07c18355d3f3fa22a
calvinsim2 <88623537+calvinsim2@users.noreply.github.com>
2024-04-17 00:31:51 +0800

    sync

--- VideoGameOnlineShop/obj/Debug/net6.0/VideoGameOnlineShopApplication.AssemblyInfo.cs
+++ VideoGameOnlineShop/obj/Debug/net6.0/VideoGameOnlineShopApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameOnlineShopApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf80170d87c4efac732bdb3489722b4df7a7335f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c507898644f6c3637c843b3b79644f0b03ca5504")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameOnlineShopApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameOnlineShopApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
