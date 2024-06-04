248548bfc3044e29a0f2dc311123c5d6cebdfe33
calvinsim2 <88623537+calvinsim2@users.noreply.github.com>
2024-06-04 12:55:39 +0800

    Added initialized parameters for DeveloperService and GameService UT fies

--- VideoGameOnlineShopUnitTest/Domain/Services/DeveloperServiceUnitTest.cs
+++ VideoGameOnlineShopUnitTest/Domain/Services/DeveloperServiceUnitTest.cs
@@ -1,6 +1,16 @@
-﻿namespace VideoGameOnlineShopUnitTest.Domain.Services
+﻿using FakeItEasy;
+using VideoGameOnlineShopDomain.Interfaces;
+using VideoGameOnlineShopDomain.Services;
+
+namespace VideoGameOnlineShopUnitTest.Domain.Services
 {
     public class DeveloperServiceUnitTest
     {
+        public static readonly IGameRepository _gameRepositoryMock = A.Fake<IGameRepository>();
+        public static readonly IDeveloperRepository _developerRepositoryMock = A.Fake<IDeveloperRepository>();
+        public static readonly IGameService _gameServiceMock = A.Fake<IGameService>();
+        public readonly DeveloperService developerService = new DeveloperService(_gameRepositoryMock, 
+                                                                                 _developerRepositoryMock,
+                                                                                 _gameServiceMock);
     }
 }
--- VideoGameOnlineShopUnitTest/Domain/Services/GameServiceUnitTest.cs
+++ VideoGameOnlineShopUnitTest/Domain/Services/GameServiceUnitTest.cs
@@ -1,6 +1,17 @@
-﻿namespace VideoGameOnlineShopUnitTest.Domain.Services
+﻿using FakeItEasy;
+using VideoGameOnlineShopDomain.Interfaces;
+using VideoGameOnlineShopDomain.Interfaces.Common;
+using VideoGameOnlineShopDomain.Services;
+
+namespace VideoGameOnlineShopUnitTest.Domain.Services
 {
     public class GameServiceUnitTest
     {
+        public static readonly IGameRepository _gameRepositoryMock = A.Fake<IGameRepository>();
+        public static readonly IDeveloperRepository _developerRepositoryMock = A.Fake<IDeveloperRepository>();
+        public static readonly ICommonUtilityMethods _commonUtilityMethodsMock = A.Fake<ICommonUtilityMethods>();
+        public readonly GameService gameService = new GameService(_gameRepositoryMock, 
+                                                                  _developerRepositoryMock,
+                                                                  _commonUtilityMethodsMock);
     }
 }
