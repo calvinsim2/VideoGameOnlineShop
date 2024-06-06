172449d6f5beb9b554c99780b5f48b142dd806cb
calvinsim2 <88623537+calvinsim2@users.noreply.github.com>
2024-06-06 22:17:24 +0800

    Added developerService GetAllDevelopers unit test

--- VideoGameOnlineShopUnitTest/Domain/Services/DeveloperServiceUnitTest.cs
+++ VideoGameOnlineShopUnitTest/Domain/Services/DeveloperServiceUnitTest.cs
@@ -12,5 +12,47 @@ namespace VideoGameOnlineShopUnitTest.Domain.Services
         public readonly DeveloperService developerService = new DeveloperService(_gameRepositoryMock, 
                                                                                  _developerRepositoryMock,
                                                                                  _gameServiceMock);
+
+        [Fact]
+        public async Task GetAllExistingDevelopersAsync_Positive_ExistingDevelopersFound_ReturnListOfDevelopers()
+        {
+            // Arrange
+            VideoGameOnlineShopDomain.DomainModels.Developer developerMock = new VideoGameOnlineShopDomain.DomainModels.Developer();
+
+            List<VideoGameOnlineShopDomain.DomainModels.Developer> developersMock = 
+                new List<VideoGameOnlineShopDomain.DomainModels.Developer>
+            {
+                developerMock
+            };
+
+            // Act
+            A.CallTo(() => _developerRepositoryMock.GetAllDevelopersAsync(A<bool>.Ignored))
+                                .WithAnyArguments().Returns(developersMock);
+
+
+            var result = await developerService.GetAllExistingDevelopersAsync();
+
+            // Assert
+            Assert.Single(result);
+        }
+
+        [Fact]
+        public async Task GetAllExistingDevelopersAsync_Negative_NoDevelopersFound_ReturnEmptyList()
+        {
+            // Arrange
+
+            List<VideoGameOnlineShopDomain.DomainModels.Developer> developersMock = 
+                new List<VideoGameOnlineShopDomain.DomainModels.Developer>();
+
+            // Act
+            A.CallTo(() => _developerRepositoryMock.GetAllDevelopersAsync(A<bool>.Ignored))
+                           .WithAnyArguments().Returns(developersMock);
+
+
+            var result = await developerService.GetAllExistingDevelopersAsync();
+
+            // Assert
+            Assert.Empty(result);
+        }
     }
 }
